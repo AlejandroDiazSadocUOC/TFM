f306b1e7033987ed2a525082e92ced75a9c8cec0
AlejandroDiazSadocUOC <alediazsadoc@uoc.edu>
2021-04-02 23:29:56 +0200

    Añadidas funcionalidades protagonista y enemigos.

--- TFM/Assets/Scripts/PlayerBehaviour.cs
+++ TFM/Assets/Scripts/PlayerBehaviour.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
 
+    public List<GameObject> m_BulletsPrefab;
+
     private Animator playerAnimator;
     private Rigidbody m_Rigidbody;
 
@@ -12,14 +14,40 @@ public class PlayerBehaviour : MonoBehaviour
     private float m_LateralMovementInputValue;
     private float m_FireVerticalInputValue;
     private float m_FireLateralInputValue;
-    private float m_Speed = 2.0f;
+
+    [SerializeField]
+    private float m_CurrentSpeed = 4.0f;
+    [SerializeField]
+    private float m_ShootCooldown = 0.25f;
+    [SerializeField]
+    private float m_CurrentHealth = 3;
+    [SerializeField]
+    private float m_MaxHealth = 3;
+
+    private int m_CurrentBullet = 0;
+    private float m_CurrentAmmo = 10.0f;
+
+    private float m_MaxAmmoOfBullet = 10.0f;
+
+    private float lastShot = 0;
 
     private int m_VerticalInvert = 1, m_LateralInvert = 1;
+
+    public static PlayerBehaviour m_instance;
+
+    private void Awake()
+    {
+        if (m_instance == null)
+            m_instance = this;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         m_Rigidbody = GetComponent<Rigidbody>();
         playerAnimator = GetComponent<Animator>();
+        lastShot = Time.time;
+        StartCoroutine(CheckAmmoCoroutine());
     }
 
     // Update is called once per frame
@@ -59,31 +87,50 @@ public class PlayerBehaviour : MonoBehaviour
 
     private void RefreshFire()
     {
-        if (m_FireVerticalInputValue > 0)
+        if (m_FireVerticalInputValue > 0 && CanShot())
         {
             transform.rotation = Quaternion.Euler(transform.rotation.x, 0.0f, transform.rotation.z);
             m_VerticalInvert = 1;
+            Fire();
+            lastShot = Time.time;
         }
-        else if (m_FireVerticalInputValue < 0)
+        else if (m_FireVerticalInputValue < 0 && CanShot())
         {
             //transform.localRotation = new Quaternion(transform.localRotation.x, 180.0f, transform.localRotation.z, transform.localRotation.w);
             transform.rotation = Quaternion.Euler(transform.rotation.x, 180.0f, transform.rotation.z);
             m_VerticalInvert = -1;
+            Fire();
+            lastShot = Time.time;
 
         }
 
-        if (m_FireLateralInputValue > 0)
+        if (m_FireLateralInputValue > 0 && CanShot())
         {
             transform.rotation = Quaternion.Euler(transform.rotation.x, 90.0f, transform.rotation.z);
             m_LateralInvert = 1;
             m_VerticalInvert = 1;
+            Fire();
+            lastShot = Time.time;
         }
-        else if (m_FireLateralInputValue < 0)
+        else if (m_FireLateralInputValue < 0 && CanShot())
         {
             transform.rotation = Quaternion.Euler(transform.rotation.x, 270.0f, transform.rotation.z);
             m_LateralInvert = -1;
             m_VerticalInvert = -1;
+            Fire();
+            lastShot = Time.time;
         }
+    }
+
+    private void Fire()
+    {
+        Instantiate(m_BulletsPrefab[m_CurrentBullet],new Vector3(transform.position.x + 0.2f,0.2f, transform.position.z), transform.rotation);
+        m_CurrentAmmo--;
+    }
+
+    private bool CanShot()
+    {
+        return Time.time - lastShot >= m_ShootCooldown && m_CurrentAmmo > 0;
     }
 
     private void ResetBoolsAnim()
@@ -102,9 +149,56 @@ public class PlayerBehaviour : MonoBehaviour
     private void Move()
     {
         // Create a vector in the direction the tank is facing with a magnitude based on the input, speed and the time between frames
-        Vector3 movement = Vector3.forward * m_MovementInputValue * m_Speed * Time.deltaTime ;
-        Vector3 lateralMovement = Vector3.right * m_LateralMovementInputValue * m_Speed * Time.deltaTime ;
+        Vector3 movement = Vector3.forward * m_MovementInputValue * m_CurrentSpeed * Time.deltaTime ;
+        Vector3 lateralMovement = Vector3.right * m_LateralMovementInputValue * m_CurrentSpeed * Time.deltaTime ;
         // Apply this movement to the rigidbody's position
         m_Rigidbody.MovePosition(m_Rigidbody.position + movement + lateralMovement);
+    }
+
+    private IEnumerator CheckAmmoCoroutine()
+    {
+        if (m_CurrentAmmo != 0)
+        {
+            m_CurrentAmmo = m_CurrentAmmo < m_MaxAmmoOfBullet ? m_CurrentAmmo + 1 : m_CurrentAmmo;
+            yield return new WaitForSeconds(0.5f);
+            StartCoroutine(CheckAmmoCoroutine());
+        }
+        else
+        {
+            m_CurrentAmmo = m_MaxAmmoOfBullet;
+            yield return new WaitForSeconds(5.0f);
+            StartCoroutine(CheckAmmoCoroutine());
+        }
+    }
+
+    public void ReduceLife(float lifeQuantity)
+    {
+        m_CurrentHealth -= lifeQuantity;
+    }
+
+    public void AddLife(float LifeQuantity)
+    {
+        m_CurrentHealth = m_CurrentHealth < m_MaxHealth ? m_CurrentHealth+LifeQuantity > m_MaxHealth ? m_MaxHealth : m_CurrentHealth + LifeQuantity : 0;
+    }
+
+    public void AddSpeed(float speed)
+    {
+        m_CurrentSpeed += speed;
+    }
+
+    public void ChangeCurrentBullet(int BulletNumber)
+    {
+        m_CurrentBullet = BulletNumber;
+    }
+
+    public void CheckDeath()
+    {
+        if (m_CurrentHealth <= 0)
+            OnDeath();
+    }
+
+    private void OnDeath()
+    {
+        Destroy(gameObject);
     }
 }
